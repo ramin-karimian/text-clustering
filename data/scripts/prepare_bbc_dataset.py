0f88296eb7de29619ace4c8a7e70ca4d41686112
raka95 <ramin.k2013@gmail.com>
2020-04-24 17:51:27 +0430

    update

--- data/scripts/prepare_bbc_dataset.py
+++ data/scripts/prepare_bbc_dataset.py
@@ -4,10 +4,8 @@ import numpy as np
 import re
 
 if __name__=="__main__":
-
-    # path = f"data/datasets/bbc/bbc_raw/business/001.txt"
-    path = f"data/datasets/bbc/bbc_raw"
-    savepath = f"data/bbc.csv"
+    path = f"../datasets/bbc/bbc_raw"
+    savepath = f"../bbc.csv"
     df = pd.DataFrame(columns=['text','class','class_label'])
     classDict = {"business":0,"entertainment":1,"politics":2,"sport":3,"tech":4}
     i = 0
@@ -21,10 +19,4 @@ if __name__=="__main__":
                 text = re.sub(r".\n+",'.  ',text)
                 df.loc[i] = text,classDict[topic],topic
                 i = i+1
-                # df.loc[i]['text'] = text
-                # df.loc[i]['class'] = classDict[topic]
-                # df.loc[i]['class_label'] = topic
     df.to_csv(savepath)
-
-    # with open(datapath,'r') as f :
-    #     data = f.read()
---
+++ feature_extraction.py
@@ -0,0 +1,32 @@
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+import pickle
+import pandas as pd
+import re
+import numpy as np
+from data.scripts.utils import *
+
+
+if __name__=="__main__":
+    # corpus = ["welcome to  to stackoverflow my friend",
+    #       "my friend, don't worry, you can get help from stackoverflow"]
+    data_version="V01"
+    datapath = f"data/bbc_preprocessed_data_{data_version}.pkl"
+    savepath = f"data/bbc_preprocessed_data_{data_version}"
+    # datapath = f"data/bbc_preprocessed_data_{data_version}.pkl"
+    df = load_data(datapath)
+    data = [" ".join(x) for x in df['tokens']]
+
+
+
+    vectorizer = TfidfVectorizer()
+    tfidf_data = vectorizer.fit_transform(data)
+    save_data(savepath+"_tfidf.pkl",tfidf_data)
+
+
+    vectorizer = CountVectorizer()
+    vectorizer.fit(data)
+    tf_data = vectorizer.transform(data)
+    save_data(savepath+"_tf.pkl",tf_data)
+
+
+
